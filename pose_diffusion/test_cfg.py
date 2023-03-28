52986a9b9d161dd3ac9e22274b5804ea1574ff25
Jianyuan Wang <jianyuan@fb.com>
2023-03-28 17:57:40 +0100

    debug gitignore

--- pose_diffusion/test_cfg.py
+++ pose_diffusion/test_cfg.py
@@ -5,11 +5,12 @@ import os
 import re
 import time
 from pathlib import Path
-
 import numpy as np
 import torch
 from typing import Dict, List, Optional, Union
-from omegaconf import OmegaConf
+from omegaconf import OmegaConf, DictConfig
+import hydra
+from hydra.utils import instantiate
 
 
 
@@ -30,7 +31,7 @@ from omegaconf import OmegaConf
 #     model = __all__[name](
 #         num_encoders=num_encoders, num_decoders=num_decoders, **kwargs
 #     )
-    
+
 #     return model
 
 
@@ -42,6 +43,8 @@ class DummyModel:
         transformer: Dict,
     ):
         self.name = name
+        import pdb;pdb.set_trace()
+
         # it is easy to eliminate model_cfg.AAA from within the modules
         # the nodes of the config system are just dicts
         # the leaf can be any type, e.g., str, list, int, and so on
@@ -51,27 +54,40 @@ class DummyModel:
         # just need to ensure the dicts (e.g., transformer) are also taken away
         # we can also call them backbone_cfg, transformer_cfg if beneficial
 
-        # the content of dicts transformer and backbone 
+        # the content of dicts transformer and backbone
         # are defined in the downsteam functions/modules
 
-        # but to be honest, in this way, 
+        # but to be honest, in this way,
         # we are somehow back close to implicitron...
-        
-        
+
         # import pdb;pdb.set_trace()
-        self.transformer = build_transformer(**transformer)
-        self.backbone = build_backbone(**backbone)
+        # self.transformer = build_transformer(**transformer)
+        # self.backbone = build_backbone(**backbone)
 
 
-def main():
-    cli_cfg = OmegaConf.from_cli()
-    cfg_file = cli_cfg.get("cfg_file")
-    yaml_cfg = OmegaConf.load(cfg_file)
+class DummyModel:
+    def __init__(
+        self,
+        name: str,
+        backbone: Dict,
+        transformer: Dict,
+    ):
+        self.name = name
 
-    cfg = OmegaConf.merge(yaml_cfg, cli_cfg)
 
-    model = DummyModel(**cfg.MODEL)
-    optimizer = DummyOptimizer(**cfg.OPTIMIZER)
+@hydra.main(config_path="cfgs/", config_name="default")
+def main(cfg: DictConfig) -> None:
+    print(OmegaConf.to_yaml(cfg))
+    model = instantiate(cfg.MODEL)
+    
+    
+    # instantiate
+    # import pdb;pdb.set_trace()
+    # TODO: The following may be needed for hydra/submitit it to work
+
+    # dump_cfg(cfg)
+    # experiment.run()
+
 
 
 if __name__ == "__main__":
