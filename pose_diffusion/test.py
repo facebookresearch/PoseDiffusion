cbe868d39fe16f6041830945d1dfb0d16453685c
Jianyuan <jianyuan@meta.com>
2023-03-30 17:03:10 -0700

    add example usage

--- pose_diffusion/test.py
+++ pose_diffusion/test.py
@@ -29,15 +29,14 @@ def main(cfg: DictConfig) -> None:
     # Loading Image
     original_cwd = get_original_cwd()  # hydra changes the default path, goes back
     folder_path = os.path.join(original_cwd, cfg.TEST.image_path)
-    image_size = cfg.TEST.image_size
-    images_tensor = load_and_preprocess_images(folder_path, image_size)
-
-    # Or randomly generated image, range from 0 to 1
-    # images_tensor = torch.rand(10, 3, 224, 224)
+    images = load_and_preprocess_images(folder_path, cfg.TEST.image_size)
+    
+    # Or randomly generated image, ranging from 0 to 1
+    # images = torch.rand(10, 3, 224, 224)
 
     # Forward
     with torch.no_grad():
-        pred_pose = model(image=images_tensor)
+        pred_pose = model(image=images)
 
     print("done")
 
